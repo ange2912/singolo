b0d2b3ffc2cf3105a97bd2eecfc83f1443350adf
Ange <genachumakov@mail.ru>
2020-03-11 19:36:16 +0300

    feat: add partial function: active tab

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+/* Header MENU */ 
 const MENU = document.getElementById('menu');
 const MENU_ITEM = MENU.querySelectorAll('li');
 MENU.addEventListener('click', (evt)=>{
     MENU_ITEM.forEach(el => el.querySelector('a').classList.remove('active'));
     evt.target.classList.add('active');
 })
+
+
+
+/* Portfolio Buttons*/
+const BUTTONS_BOX = document.getElementById('portfolio-buttons');
+const BUTTONS = BUTTONS_BOX.querySelectorAll('button');
+BUTTONS_BOX.addEventListener('click', (evt)=>{
+    BUTTONS.forEach(el => el.classList.remove('active'));
+    evt.target.classList.add('active');
+})
+
+/*const SUBMIT = document.getElementById('submit');
+SUBMIT.addEventListener('click', (evt)=>{
+    evt.preventDefault();
+})*/
