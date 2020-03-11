19e603ba5259c9b832c5765781bc486d7867df74
Ange <genachumakov@mail.ru>
2020-03-11 23:58:51 +0300

    feat: complete - active Tab in portfolio

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/* Header MENU */ 
+/* Header MENU */
 const MENU = document.getElementById('menu');
 const MENU_ITEM = MENU.querySelectorAll('li');
-MENU.addEventListener('click', (evt)=>{
+MENU.addEventListener('click', (evt) => {
     MENU_ITEM.forEach(el => el.querySelector('a').classList.remove('active'));
     evt.target.classList.add('active');
 })
@@ -11,8 +11,28 @@ MENU.addEventListener('click', (evt)=>{
 /* Portfolio Buttons*/
 const BUTTONS_BOX = document.getElementById('portfolio-buttons');
 const BUTTONS = BUTTONS_BOX.querySelectorAll('button');
-BUTTONS_BOX.addEventListener('click', (evt)=>{
+
+
+BUTTONS_BOX.addEventListener('click', (evt) => {
     BUTTONS.forEach(el => el.classList.remove('active'));
+
+    const IMAGES_BOX = document.getElementById('images_box');
+    const IMAGES = IMAGES_BOX.querySelectorAll('img');
+    const TAB1 = [];
+    for (let i = 0; i < IMAGES.length; i++) {
+        let currentImage;
+        if (i === IMAGES.length - 1) {
+            currentImage = IMAGES[0];
+        } else {
+            currentImage = IMAGES[i + 1];
+        }
+        TAB1.push(currentImage);
+    }
+    IMAGES_BOX.innerHTML = '';
+    for (let i = 0; i < IMAGES.length; i++) {
+        IMAGES_BOX.append(TAB1[i]);
+    }
+
     evt.target.classList.add('active');
 })
 
